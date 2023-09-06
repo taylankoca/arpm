20e0beead433cf671e0e55eefdb33dbde793de88
Taylan Koca <taylankoca@gmail.com>
2023-09-06 16:37:07 +0000

    Controller bug fixed.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,18 +4,18 @@ class PageController {
     public function home()
     {
         $content = "I'm in the homepage";
-        require 'views/view.php';
+        require 'resources/view.php';
     }
 
     public function folder1()
     {
         $content = "I'm in folder1";
-        require 'views/view.php';
+        require 'resources/view.php';
     }
 
     public function folder2()
     {
         $content = "I'm in folder2";
-        require 'views/view.php';
+        require 'resources/view.php';
     }
 }
